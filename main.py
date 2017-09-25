c54cc2cbfbf23fcdef0aaec5e34fb08fdb70f144
Satoshi Watanabe <sassy.watanabe@gmail.com>
2017-09-25 23:47:55 +0900

    add loop method.

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from selenium import webdriver
+import time
 
 URL = "https://www.instagram.com/?hl=ja"
+WAIT_TIME = 1800
 
-def like_instagram(username, password):
-    driver = webdriver.Chrome('./chromedriver')
-    driver.get(URL)
-    driver.find_element_by_class_name('_b93kq').click()
-    driver.find_element_by_name('username').send_keys(username)
-    driver.find_element_by_name('password').send_keys(password)
-    driver.implicitly_wait(2)
-    driver.find_elements_by_tag_name('button')[0].submit()
-    tags = driver.find_elements_by_class_name('coreSpriteHeartOpen')
-    for tag in tags:
-        tag.click()
+class InstagramAgent():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-    driver.quit()
+    def like_instagram(self):
+        driver = webdriver.Chrome('./chromedriver')
+        driver.get(URL)
+        driver.find_element_by_class_name('_b93kq').click()
+        driver.find_element_by_name('username').send_keys(self.username)
+        driver.find_element_by_name('password').send_keys(self.password)
+        driver.implicitly_wait(2)
+        driver.find_elements_by_tag_name('button')[0].submit()
+        tags = driver.find_elements_by_class_name('coreSpriteHeartOpen')
+        for tag in tags:
+            tag.click()
+        driver.quit()
+
+    def like_instagram_loop(self, wait_time=WAIT_TIME):
+        while True:
+            self.like_instagram()
+            time.sleep(wait_time)
 
 
 if __name__ == '__main__':
     import fire
-    fire.Fire(like_instagram)
+    fire.Fire(InstagramAgent)
